571123dad036fa1c9552aa958aa38f6fc209bbf3
Jim BENNA <jim.benna@gmail.com>
2025-03-06 23:36:19 +0100

    Nice indentation

--- FW_API_Grab_URLGroups.ps1
+++ FW_API_Grab_URLGroups.ps1
@@ -33,7 +33,7 @@ function Split-StringAfterEqualSign {
         }
 
         $splitString = $inputString -split "="
-#        write-host $splitString[1]
+        #        write-host $splitString[1]
         if (($null -eq $splitString[1]) -or ($splitString[1] -eq "")) {
             throw "No value after '=' for $splitString"
         } 
@@ -72,9 +72,9 @@ function TranformInterfacesXmlListToArray {
     $XmlTag = $XmlDocument.SelectNodes("//WebFilterURLGroup")
     $OutTagArray = @()
     foreach ($Node in $XmlTag) {
-            $OutTagArray += [pscustomobject]@{            
+        $OutTagArray += [pscustomobject]@{            
             Name        = $Node.Name
-            XmlUrlList = $Node.URLlist.URL
+            XmlUrlList  = $Node.URLlist.URL
             Description = $Node.Description
             IsDefault   = $Node.IsDefault
         }
@@ -82,8 +82,7 @@ function TranformInterfacesXmlListToArray {
     return $OutTagArray
 }
 # MAIN PROGRAM
-try 
-{
+try {
     if (($null -eq $Param01) -or ($Param01 -eq "")) {
         Write-Host "   No input firewalls list has been provided   " -ForegroundColor Red -BackgroundColor Yellow -NoNewline
         write-host""
@@ -117,71 +116,62 @@ try
         #        Write-Host "Second Parameter name : "$Input02Name
         #        Write-Host "Output Filename       : "$Input02Value        
         $OutputFile = $Input02Value
-        if (Test-Path -Path $InputFile) 
-        {
+        if (Test-Path -Path $InputFile) {
             # Input file exists, we can continue
-        try 
-        {
-            $file = Get-Item $InputFile
-            $file.OpenRead().Close()
-            $ImportJsonFile = Get-content -Path $InputFile | ConvertFrom-Json
-            $Counter = 0
-            $MainTable = [System.Collections.ArrayList]::new()
-                foreach ($Item in $ImportJsonFile) 
-                {
-                    try 
-                    {
-                    Write-Host "---------------------------------------------------------"
-                    Write-Host "Iteration Number           :"$Counter
-                    $FwAdminIpAddress = $Item.IPAddress
-                    Write-Host "IP Address                 :"$FwAdminIpAddress
-                    $FwAdminListeningPort = $Item.AccesPortNb
-                    Write-Host "Port Number                :"$FwAdminListeningPort
-                    $EncryptedPassword = $Item.Password
-                    $Password = ConvertTo-SecureString -String $EncryptedPassword
-                    $Credentials = New-Object System.Management.Automation.PSCredential ($Item.LoginName, $Password)
-                    Write-Host "Credentials Login name     : $($Credentials.UserName)"
-                    Write-Host "Credentials Login Password : $($Credentials.GetNetworkCredential().Password)"
-                    $AccessTimeOut = $Item.TimeOut
-                    Write-Host "Access TimeOut             :"$AccessTimeOut
-                    $FuncURL = BuildURLFunction -FuncFwIP $FwAdminIpAddress -FuncFwPort $FwAdminListeningPort -FuncFwLogin $($Credentials.UserName) -FuncFwPwd $($Credentials.GetNetworkCredential().Password)
-                    Write-Host $FuncURL
-                        try 
-                        {
-                        $HttpResult = (Invoke-RestMethod -Uri $FuncURL -Method Post -ContentType "application/xml" -SkipCertificateCheck -TimeoutSec $AccessTimeOut)
-                        $EntriesListArray = TranformInterfacesXmlListToArray -XmlDocument $HttpResult
-                        $Firewalls_Object = [PSCustomObject]@{
-                            Firewall        = $Item.IPAddress
-                            FirewallURLS = $EntriesListArray
+            try {
+                $file = Get-Item $InputFile
+                $file.OpenRead().Close()
+                $ImportJsonFile = Get-content -Path $InputFile | ConvertFrom-Json
+                $Counter = 0
+                $MainTable = [System.Collections.ArrayList]::new()
+                foreach ($Item in $ImportJsonFile) {
+                    try {
+                        Write-Host "---------------------------------------------------------"
+                        Write-Host "Iteration Number           :"$Counter
+                        $FwAdminIpAddress = $Item.IPAddress
+                        Write-Host "IP Address                 :"$FwAdminIpAddress
+                        $FwAdminListeningPort = $Item.AccesPortNb
+                        Write-Host "Port Number                :"$FwAdminListeningPort
+                        $EncryptedPassword = $Item.Password
+                        $Password = ConvertTo-SecureString -String $EncryptedPassword
+                        $Credentials = New-Object System.Management.Automation.PSCredential ($Item.LoginName, $Password)
+                        Write-Host "Credentials Login name     : $($Credentials.UserName)"
+                        Write-Host "Credentials Login Password : $($Credentials.GetNetworkCredential().Password)"
+                        $AccessTimeOut = $Item.TimeOut
+                        Write-Host "Access TimeOut             :"$AccessTimeOut
+                        $FuncURL = BuildURLFunction -FuncFwIP $FwAdminIpAddress -FuncFwPort $FwAdminListeningPort -FuncFwLogin $($Credentials.UserName) -FuncFwPwd $($Credentials.GetNetworkCredential().Password)
+                        Write-Host $FuncURL
+                        try {
+                            $HttpResult = (Invoke-RestMethod -Uri $FuncURL -Method Post -ContentType "application/xml" -SkipCertificateCheck -TimeoutSec $AccessTimeOut)
+                            $EntriesListArray = TranformInterfacesXmlListToArray -XmlDocument $HttpResult
+                            $Firewalls_Object = [PSCustomObject]@{
+                                Firewall     = $Item.IPAddress
+                                FirewallURLS = $EntriesListArray
+                            }
+                            #                        $Firewalls_Object
+                            $MainTable.add($Firewalls_Object) | Out-Null
                         }
-#                        $Firewalls_Object
-                        $MainTable.add($Firewalls_Object) | Out-Null
-                        }
-                        catch 
-                        {
-                         Write-host "Error calling URL"
-                         Write-Host "Error : $($_.Exception.Message)"
+                        catch {
+                            Write-host "Error calling URL"
+                            Write-Host "Error : $($_.Exception.Message)"
                         }
                     }
-                    catch 
-                    {
-                    Write-Host "Error encountered while parsing "$InputFile
-                    Write-Host "Error $($_.Exception.Message)"
-                    exit 1
+                    catch {
+                        Write-Host "Error encountered while parsing "$InputFile
+                        Write-Host "Error $($_.Exception.Message)"
+                        exit 1
                     }
                 }            
                 #            Write-Host ""
                 $Counter++
-                            Write-Host "Compteur :" $Counter
+                Write-Host "Compteur :" $Counter
             }
-        catch 
-        {
+            catch {
                 Write-Host "File "$FullFileName" exists but can not be accessed in Read mode"
                 exit 2               
+            }
         }
-        }
-        else 
-        {
+        else {
             # Input file does not exist exit program
             Write-Host "File "$InputFile" does not exist"
             exit 3
@@ -189,8 +179,7 @@ try
     }
     
 }
-catch 
-{
+catch {
     write-host ""
     Write-Error "An error occurred: $_"
     write-host ""
