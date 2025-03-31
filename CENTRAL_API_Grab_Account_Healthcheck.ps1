8fc9c4827f7389f86cc0617b1acc406572540081
Jim Benna <jim.benna@gmail.com>
2025-03-31 17:13:59 +0200

    Retrieve Health Status and display the result

--- CENTRAL_API_Grab_Account_Healthcheck.ps1
+++ CENTRAL_API_Grab_Account_Healthcheck.ps1
@@ -1,44 +1,68 @@
-param ([switch] $SaveCredentials)
-<#
-    Description: Authentication Script for Sophos Central
-    Parameters: -SaveCredentials -> will store then entered credentials locally on the PC, this is needed when
-                                    running the script unattended
-#>
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$ParamClientId = "",
 
+    [Parameter(Mandatory=$true)]
+    [string]$ParamClientSecret = ""
+)
+
+function Split-StringAfterEqualSign {
+    param (
+        [string]$inputString
+    )
+
+    try {
+        if (-not $inputString.Contains("=")) {
+            throw "Input string does not contain an '=' sign."
+        }
+
+        $splitString = $inputString -split "="
+        return @{
+            Key = $splitString[0]
+            Value = $splitString[1]
+        }
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
+}
 Clear-Host
+try {
+    if (($null -eq $ParamClientId) -or ($ParamClientId -eq "")) {
+        Write-Output "No Client Id provided"
+        {break}
+    }
+        if (($null -eq $ParamClientSecret) -or ($ParamClientSecret -eq "")){
+        Write-Output "No Client Secret provided"
+        {break}
+    }
+    else {
+        $resultClient = Split-StringAfterEqualSign -inputString $ParamClientId
+        #Write-Host "Id Client     : "$resultClient.Key
+        #Write-Host "Client ID     : "$resultClient.Value
+        $ClientId=$resultClient.Value
+        $resultSecret = Split-StringAfterEqualSign -inputString $ParamClientSecret
+        #Write-Host "Id Secret     : "$resultSecret.Key
+        #Write-Host "Client Secret : "$resultSecret.Value
+        $ClientSecret=$resultSecret.Value
+    }
+} catch {
+    Write-Error "A basic error occurred: $_"
+    exit 1
+}
 Write-Output "==============================================================================="
 Write-Output "Sophos CENTRAL API - Account HealtCheck"
 Write-Output "==============================================================================="
-
-# Define the filename and path for the credential file
-$CredentialFile = $PSScriptRoot + '\Sophos_Central_Admin_Credentials.json'
-
-# Check if Central API Credentials have been stored, if not then prompt the user to enter the credentials
-if (((Test-Path $CredentialFile) -eq $false) -or $SaveCredentials){
-	# Prompt for Credentials
-	$ClientId = Read-Host "Please Enter your Client ID"
-    if ($ClientID -eq "") {Break}
-	$ClientSecret = Read-Host "Please Enter your Client Secret" -AsSecureString
-} else { 
-    # Read Credentials from JSON File
-    $Credentials = Get-Content $CredentialFile | ConvertFrom-Json
-    $ClientId = $Credentials[0]
-    $ClientSecret = $Credentials[1] | ConvertTo-SecureString
-}
-
-# We are making use of the PSCredentials object to store the API credentials
-# The Client Secret will be encrypted for the user excuting the script
-# When scheduling execution of the script remember to use the same user context
-$SecureCredentials = New-Object System.Management.Automation.PSCredential -ArgumentList $ClientId , $ClientSecret
-
 # SOPHOS OAuth URL
 $AuthURI = "https://id.sophos.com/api/v2/oauth2/token"
 
 # Body and Header for oAuth2 Authentication
 $AuthBody = @{}
 $AuthBody.Add("grant_type", "client_credentials")
-$AuthBody.Add("client_id", $SecureCredentials.GetNetworkCredential().Username)
-$AuthBody.Add("client_secret", $SecureCredentials.GetNetworkCredential().Password)
+$AuthBody.Add("client_id", $ClientId)
+$AuthBody.Add("client_secret", $ClientSecret)
+#$AuthBody.Add("client_id", $SecureCredentials.GetNetworkCredential().Username)
+#$AuthBody.Add("client_secret", $SecureCredentials.GetNetworkCredential().Password)
 $AuthBody.Add("scope", "token")
 $AuthHead = @{}
 $AuthHead.Add("content-type", "application/x-www-form-urlencoded")
@@ -60,7 +84,7 @@ try {
     Write-Output "Please verify the credentials used!" 
     Write-Output "" 
     Read-Host -Prompt "Press ENTER to continue..."
-    exit 1
+    exit 2
 }
 
 # Set the Token for use later on:
@@ -86,11 +110,6 @@ if ($Result.idType -ne "tenant") {
 # Save Response details
 $TenantID = $Result.id
 $DataRegion = $Result.ApiHosts.dataRegion
-
-################# INSERT CODE HERE ###############
-# add this code snippet to one of the auth code samples for Central Admin, Central Enterprise Dashboard or Central Partner (snippets 1 2 or 3)
-# you will find a line that says INSERT CODE HERE
-
 # Tenant Headers:
 $TenantHead = @{}
 $TenantHead.Add("Authorization" ,"Bearer $Token")
@@ -128,4 +147,4 @@ Write-Host ("`nTamper Protection")
 Write-Host ("-------------------------------")
 Write-Host ("Tamper Protection enabled for account: " + $Result.endpoint.tamperProtection.global)
 Write-Host ("Computers with disabled Tamper Protection: " + $Result.endpoint.tamperProtection.computer.disabled + " out of " + $Result.endpoint.tamperProtection.computer.total )
-Write-Host ("Servers  with disabled Tamper Protection: " + $Result.endpoint.tamperProtection.server.disabled + " out of " + $Result.endpoint.tamperProtection.server.total)  @{label='Central Connection';e={$_.status.connected}}
+Write-Host ("Servers  with disabled Tamper Protection: " + $Result.endpoint.tamperProtection.server.disabled + " out of " + $Result.endpoint.tamperProtection.server.total)
