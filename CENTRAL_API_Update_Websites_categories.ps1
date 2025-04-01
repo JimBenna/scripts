40e7b0ab1da4c50ad007280d65bfa571fcb70a4e
Jim Benna <jim.benna@gmail.com>
2025-04-01 15:57:21 +0200

    correction of typo error and 1st shot of websites management

---
+++ CENTRAL_API_Update_Websites_categories.ps1
@@ -0,0 +1,163 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$ParamClientId = "",
+
+    [Parameter(Mandatory=$true)]
+    [string]$ParamClientSecret = ""
+)
+
+function Split-StringAfterEqualSign {
+    param (
+        [string]$inputString
+    )
+
+    try {
+        if (-not $inputString.Contains("=")) {
+            throw "Input string does not contain an '=' sign."
+        }
+
+        $splitString = $inputString -split "="
+        return @{
+            Key = $splitString[0]
+            Value = $splitString[1]
+        }
+    }
+    catch {
+        Write-Error "An error occurred: $_"
+    }
+}
+Clear-Host
+try {
+    if (($null -eq $ParamClientId) -or ($ParamClientId -eq "")) {
+        Write-Output "No Client Id provided"
+        {break}
+    }
+        if (($null -eq $ParamClientSecret) -or ($ParamClientSecret -eq "")){
+        Write-Output "No Client Secret provided"
+        {break}
+    }
+    else {
+        $resultClient = Split-StringAfterEqualSign -inputString $ParamClientId
+        #Write-Host "Id Client     : "$resultClient.Key
+        #Write-Host "Client ID     : "$resultClient.Value
+        $ClientId=$resultClient.Value
+        $resultSecret = Split-StringAfterEqualSign -inputString $ParamClientSecret
+        #Write-Host "Id Secret     : "$resultSecret.Key
+        #Write-Host "Client Secret : "$resultSecret.Value
+        $ClientSecret=$resultSecret.Value
+    }
+} catch {
+    Write-Error "A basic error occurred: $_"
+    exit 1
+}
+Write-Output "==============================================================================="
+Write-Output "Sophos CENTRAL API - Setup Websites and category"
+Write-Output "==============================================================================="
+#CSV filename and full directory
+$ScriptLaunchDate= Get-Date -Format "yyyyMMddHHmmssfff"
+$CSV_Endpoints_list = "Websites_list_$ScriptLaunchDate.csv"
+# SOPHOS OAuth URL
+$AuthURI = "https://id.sophos.com/api/v2/oauth2/token"
+
+# Body and Header for oAuth2 Authentication
+$AuthBody = @{}
+$AuthBody.Add("grant_type", "client_credentials")
+$AuthBody.Add("client_id", $ClientId)
+$AuthBody.Add("client_secret", $ClientSecret)
+#$AuthBody.Add("client_id", $SecureCredentials.GetNetworkCredential().Username)
+#$AuthBody.Add("client_secret", $SecureCredentials.GetNetworkCredential().Password)
+$AuthBody.Add("scope", "token")
+$AuthHead = @{}
+$AuthHead.Add("content-type", "application/x-www-form-urlencoded")
+
+# Set TLS Version
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Post Request to SOPHOS for OAuth2 token
+try {
+    $Result = (Invoke-RestMethod -Method Post -Uri $AuthURI -Body $AuthBody -Headers $AuthHead -ErrorAction SilentlyContinue -ErrorVariable ScriptError)
+    if ($SaveCredentials) {
+	    $ClientSecret = $ClientSecret | ConvertFrom-SecureString
+	    ConvertTo-Json $ClientID, $ClientSecret | Out-File $CredentialFile -Force
+    }
+} catch {
+    # If there's an error requesting the token, say so, display the error, and break:
+    Write-Output "" 
+	Write-Output "AUTHENTICATION FAILED - Unable to retreive SOPHOS API Authentication Token"
+    Write-Output "Please verify the credentials used!" 
+    Write-Output "" 
+    Write-Output "If you are working with saved credentials then you can reset them by calling"
+    Write-Output "this script with the -SaveCredentials parameter"
+    Write-Output "" 
+    Read-Host -Prompt "Press ENTER to continue..."
+    Break
+}
+
+# Set the Token for use later on:
+$Token = $Result.access_token
+
+# SOPHOS Whoami URI:
+$WhoamiURI = "https://api.central.sophos.com/whoami/v1"
+
+# SOPHOS Whoami Headers:
+$WhoamiHead = @{}
+$WhoamiHead.Add("Content-Type", "application/json")
+$WhoamiHead.Add("Authorization", "Bearer $Token")
+
+# Post Request to SOPHOS for Whoami Details:
+$Result = (Invoke-RestMethod -Method Get -Uri $WhoamiURI -Headers $WhoamiHead -ErrorAction SilentlyContinue -ErrorVariable ScriptError)
+
+# Check if we are using tenant (Central Admin) credentials
+if ($Result.idType -ne "tenant") {
+    Write-Output "Aborting script - idType does not match tenant!"
+    Break
+}
+
+# Save Response details
+$TenantID = $Result.id
+$DataRegion = $Result.ApiHosts.dataRegion
+
+################# INSERT CODE HERE ###############
+# add this code snippet to one of the auth code samples for Central Admin, Central Enterprise Dashboard or Central Partner (snippets 1 2 or 3)
+# you will find a line that says INSERT CODE HERE
+# add this code snippet to one of the auth code samples for Central Admin, Central Enterprise Dashboard or Central Partner (snippets 1 2 or 3)
+# you will find a line that says INSERT CODE HERE
+
+# SOPHOS Endpoint API Headers:
+$TenantHead = @{}
+$TenantHead.Add("Authorization" ,"Bearer $Token")
+$TenantHead.Add("X-Tenant-ID" ,"$TenantID")
+$TenantHead.Add("Content-Type", "application/json")
+
+# Define URI for updating the local site list
+$Uri = $DataRegion+"/endpoint/v1/settings/web-control/local-sites"
+
+
+# Import data from CSV
+Write-Output "Importing sites from CSV..."
+Write-Output ""
+$importFile = Import-Csv $PSScriptRoot\websites.csv
+
+# Iterate through all sites from CSV
+Write-Output "Creating local sites in Sophos Central..."
+Write-Output ""
+
+foreach ($Item in $ImportFile){
+
+    # Split string in case of multiple tags
+    $Tags = @($Item.tags.split("{;}"))
+
+    # Change tags into an array
+    $Item.PSobject.Properties.Remove('tags')
+	$Item | Add-Member -NotePropertyName tags -NotePropertyValue $Tags
+    
+    # Create request body by converting to JSON
+    $Body = $Item | ConvertTo-Json
+
+    # Invoke Request
+    $Result = (Invoke-RestMethod -Uri $Uri -Method Post -ContentType "application/json" -Headers $TenantHead -Body $Body -ErrorAction SilentlyContinue -ErrorVariable ScriptError)
+    Write-Output "Created Site: $($Result.url) with ID $($Result.id)"
+    
+}
+Write-Output ""
+Write-Output "Successfully created websites and category in Sophos Central..."
